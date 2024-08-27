59a5ddce6e8fed431c9cedbb8aafe10cd3cd593e
Vladimir Volek <volek.vladimir@gmail.com>
2024-08-27 17:59:39 +0200

    feat: node connection

--- src/node.rs
+++ src/node.rs
@@ -1,7 +1,8 @@
-use pallas_network::facades::PeerClient;
-// use pallas_network::miniprotocols::txsubmission::{EraTxBody, TxIdAndSize};
-
 use crate::errors::BlockfrostError;
+use pallas_network::{
+    facades::PeerClient,
+    // miniprotocols::txsubmission::{EraTxBody, TxIdAndSize},
+};
 
 pub struct Node {
     network_magic: u64,
@@ -20,4 +21,38 @@ impl Node {
             network_magic,
         })
     }
+
+    // /// Submits a transaction to the connected Cardano node.
+    // pub async fn submit_transaction(tx_bytes: Vec<u8>) -> Result<u32, Box<dyn std::error::Error>> {
+    //     let tx_hash = pallas_crypto::hash::Hash::from(tx_bytes.clone());
+    //     let mempool = vec![(tx_hash, tx_bytes)];
+    //     let client_txsub = self.client.txsubmission();
+
+    //     // Initialize the transaction submission protocol
+    //     client_txsub.send_init().await.unwrap();
+
+    //     // Send the transaction ID and size to the node
+    //     let ids_and_size: Vec<TxIdAndSize> = mempool
+    //         .iter()
+    //         .map(|(h, b)| {
+    //             TxIdAndSize(
+    //                 pallas_network::miniprotocols::txsubmission::EraTxId(4, h.clone()),
+    //                 b.len() as u32,
+    //             )
+    //         })
+    //         .collect();
+
+    //     client_txsub.reply_tx_ids(ids_and_size).await.unwrap();
+
+    //     // Send the actual transaction to the node
+    //     let txs_to_send: Vec<EraTxBody> =
+    //         mempool.into_iter().map(|(_, b)| EraTxBody(4, b)).collect();
+    //     client_txsub.reply_txs(txs_to_send).await.unwrap();
+
+    //     // Wait for acknowledgment from the node
+    //     match client_txsub.next_request().await.unwrap() {
+    //         pallas_network::miniprotocols::txsubmission::Request::TxIds(ack, _) => Ok(ack),
+    //         _ => Err("Unexpected response from node".into()),
+    //     }
+    // }
 }
