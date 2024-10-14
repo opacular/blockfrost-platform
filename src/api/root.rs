f9ab535e3073f966dbd61b5bc1eb23e352751357
Vladimir Volek <volek.vladimir@gmail.com>
2024-10-14 18:14:41 +0200

    chore: refactor root a bit

--- src/api/root.rs
+++ src/api/root.rs
@@ -16,25 +16,25 @@ pub struct Response {
 }
 
 pub async fn route(Extension(node): Extension<Arc<RwLock<Node>>>) -> Json<Response> {
-    let mut node = node.write().await;
-    let version = node.version().await;
+    let node_version_result = {
+        let mut node = node.write().await;
+        node.version().await
+    };
 
-    let mut found_errors = Vec::new();
-    let version = version.unwrap_or_else(|_| {
-        found_errors.push("Failed to determine node version".to_string());
-        "unknown".to_string()
-    });
+    let (node_version, errors) = match node_version_result {
+        Ok(version) => (version, None),
+        Err(_) => (
+            "unknown".to_string(),
+            Some(vec!["Failed to determine node version".to_string()]),
+        ),
+    };
 
     let response = Response {
         name: "blockfrost-platform".to_string(),
         version: env!("CARGO_PKG_VERSION").to_string(),
-        node_version: version,
-        healthy: found_errors.is_empty(),
-        errors: if found_errors.is_empty() {
-            None
-        } else {
-            Some(found_errors)
-        },
+        node_version,
+        healthy: errors.is_none(),
+        errors,
     };
 
     Json(response)
