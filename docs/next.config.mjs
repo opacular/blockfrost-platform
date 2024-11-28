8e60e29a7198dc5d19f264fe88b8705a5dd84262
Michal Rus <michal.rus@iohk.io>
2024-11-28 11:29:07 +0100

    feat: handle subprocess lifetimes correctly

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,19 +1,19 @@
-import nextra from 'nextra'
- 
+import nextra from "nextra";
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   images: {
-    unoptimized: true // mandatory, otherwise won't export
-  }
+    unoptimized: true, // mandatory, otherwise won't export
+  },
   // Optional: Change the output directory `out` -> `dist`
   // distDir: "build"
-}
+};
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx'
-})
- 
-export default withNextra(nextConfig)
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+export default withNextra(nextConfig);
--- src/cbor/fallback_decoder.rs
+++ src/cbor/fallback_decoder.rs
@@ -1,5 +1,9 @@
 use std::io::{BufRead, BufReader, Write};
 use std::process as proc;
+use std::sync::{
+    atomic::{self, AtomicU32},
+    Arc,
+};
 use std::thread;
 use tokio::sync::{mpsc, oneshot};
 use tracing::error;
@@ -7,6 +11,7 @@ use tracing::error;
 #[derive(Clone)]
 pub struct FallbackDecoder {
     sender: mpsc::Sender<FDRequest>,
+    current_child_pid: Arc<AtomicU32>,
 }
 
 struct FDRequest {
@@ -17,6 +22,8 @@ struct FDRequest {
 impl FallbackDecoder {
     /// Starts a new child process.
     pub fn spawn() -> Self {
+        let current_child_pid = Arc::new(AtomicU32::new(u32::MAX));
+        let current_child_pid_clone = current_child_pid.clone();
         let (sender, mut receiver) = mpsc::channel::<FDRequest>(128);
 
         thread::spawn(move || {
@@ -24,7 +31,11 @@ impl FallbackDecoder {
             let mut last_unfulfilled_request: Option<FDRequest> = None;
 
             loop {
-                let single_run = Self::behavior(&mut receiver, &mut last_unfulfilled_request);
+                let single_run = Self::spawn_child(
+                    &mut receiver,
+                    &mut last_unfulfilled_request,
+                    &current_child_pid_clone,
+                );
                 let restart_delay = std::time::Duration::from_secs(1);
                 error!(
                     "FallbackDecoder: will restart in {:?} because of a subprocess error: {:?}",
@@ -34,30 +45,86 @@ impl FallbackDecoder {
             }
         });
 
-        Self { sender }
+        Self {
+            sender,
+            current_child_pid,
+        }
     }
 
-    fn behavior(
+    /// Decodes a CBOR error using the child process.
+    pub async fn decode(&self, cbor: &[u8]) -> Result<serde_json::Value, String> {
+        let (response_tx, response_rx) = oneshot::channel();
+        self.sender
+            .send(FDRequest {
+                cbor: cbor.to_vec(),
+                response_tx,
+            })
+            .await
+            .map_err(|err| format!("FallbackDecoder: failed to send request: {:?}", err))?;
+
+        response_rx.await.map_err(|err| {
+            format!(
+                "FallbackDecoder: worker thread dropped (won’t happen): {:?}",
+                err
+            )
+        })?
+    }
+
+    /// Returns the current child PID:
+    pub fn child_pid(&self) -> Option<u32> {
+        match self.current_child_pid.load(atomic::Ordering::Relaxed) {
+            u32::MAX => None,
+            pid => Some(pid),
+        }
+    }
+
+    fn spawn_child(
         receiver: &mut mpsc::Receiver<FDRequest>,
         last_unfulfilled_request: &mut Option<FDRequest>,
+        current_child_pid: &Arc<AtomicU32>,
     ) -> Result<(), String> {
         // FIXME: _find_ the exe_path
         // FIXME: make a release with LineBuffering
         let exe_path = "/nix/store/4y2jqhw3c2i407m8rmkvlja9wdr1kqhq-testgen-hs-exe-testgen-hs-x86_64-unknown-linux-musl-10.1.2.1/bin/testgen-hs";
 
-        let child = proc::Command::new(exe_path)
+        let mut child = proc::Command::new(exe_path)
             .arg("deserialize-stream")
             .stdin(proc::Stdio::piped())
             .stdout(proc::Stdio::piped())
             .spawn()
             .map_err(|err| format!("couldn’t start the child: {:?}", err))?;
 
-        let mut stdin = child.stdin.ok_or("couldn’t grab stdin".to_string())?;
-        let stdout = child.stdout.ok_or("couldn’t grab stdout".to_string())?;
+        current_child_pid.store(child.id(), atomic::Ordering::Relaxed);
+
+        let result = Self::process_requests(&mut child, receiver, last_unfulfilled_request);
+
+        child
+            .wait()
+            .map_err(|err| format!("couldn’t reap the child: {:?}", err))?;
+
+        result
+    }
+
+    fn process_requests(
+        child: &mut proc::Child,
+        receiver: &mut mpsc::Receiver<FDRequest>,
+        last_unfulfilled_request: &mut Option<FDRequest>,
+    ) -> Result<(), String> {
+        let stdin = child
+            .stdin
+            .as_mut()
+            .ok_or("couldn’t grab stdin".to_string())?;
+        let stdout = child
+            .stdout
+            .as_mut()
+            .ok_or("couldn’t grab stdout".to_string())?;
         let stdout_reader = BufReader::new(stdout);
         let mut stdout_lines = stdout_reader.lines();
 
-        while let Some(request) = last_unfulfilled_request.take().or(receiver.blocking_recv()) {
+        while let Some(request) = last_unfulfilled_request
+            .take()
+            .or_else(|| receiver.blocking_recv())
+        {
             let cbor_hex = hex::encode(&request.cbor);
             *last_unfulfilled_request = Some(request);
 
@@ -70,10 +137,11 @@ impl FallbackDecoder {
                 None => Err("no output from subprocess".to_string())?,
             };
 
+            // unwrap is safe, we wrote there right before the writeln!()
             let request = last_unfulfilled_request.take().unwrap();
 
             // unwrap is safe, the other side would have to drop for a
-            // panic – can’t happen:
+            // panic – can’t happen, because we control it:
             request.response_tx.send(result).unwrap();
         }
 
@@ -102,35 +170,17 @@ impl FallbackDecoder {
                     .ok_or_else(|| "Missing 'json' field".to_string())
             })
     }
-
-    /// Decodes a CBOR error using the child process.
-    pub async fn decode(&self, cbor: &[u8]) -> Result<serde_json::Value, String> {
-        let (response_tx, response_rx) = oneshot::channel();
-        self.sender
-            .send(FDRequest {
-                cbor: cbor.to_vec(),
-                response_tx,
-            })
-            .await
-            .map_err(|err| format!("FallbackDecoder: failed to send request: {:?}", err))?;
-
-        response_rx.await.map_err(|err| {
-            format!(
-                "FallbackDecoder: worker thread dropped (won’t happen): {:?}",
-                err
-            )
-        })?
-    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[tokio::test]
+    #[tracing_test::traced_test]
     async fn test_deserialization() {
-        let wrapper = FallbackDecoder::spawn();
+        let decoder = FallbackDecoder::spawn();
         let input = hex::decode("8182068182028200a0").unwrap();
-        let result = wrapper.decode(&input).await;
+        let result = decoder.decode(&input).await;
         assert_eq!(
             result,
             Ok(serde_json::json!({
@@ -151,12 +201,16 @@ mod tests {
             }))
         );
 
-        tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+        // Now, kill our child to test the restart logic:
+        sysinfo::System::new_all()
+            .process(sysinfo::Pid::from_u32(decoder.child_pid().unwrap()))
+            .unwrap()
+            .kill();
 
-        // FIXME: zombie process and hangs, when we kill it now
+        tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
 
         let input = hex::decode("8182068183051a000c275b1a000b35ec").unwrap();
-        let result = wrapper.decode(&input).await;
+        let result = decoder.decode(&input).await;
         assert_eq!(
             result,
             Ok(serde_json::json!({
