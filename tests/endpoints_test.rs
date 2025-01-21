bfd506ea72cda06b675206352832b2c21df80a49
Vladimir Volek <volek.vladimir@gmail.com>
2025-01-21 01:18:28 +0100

    tests: submit error
    chore: try CI
    
    chore: try CI

--- tests/endpoints_test.rs
+++ tests/endpoints_test.rs
@@ -1,7 +1,7 @@
+#[path = "common.rs"]
 mod common;
 
-#[cfg(test)]
-mod root_tests {
+mod tests {
     use crate::common::{build_app, initialize_logging};
     use axum::{
         body::{to_bytes, Body},
@@ -9,9 +9,10 @@ mod root_tests {
     };
     use blockfrost_platform::api::root::RootResponse;
     use pretty_assertions::assert_eq;
-    use reqwest::StatusCode;
+    use reqwest::{Method, StatusCode};
     use tower::ServiceExt;
 
+    // Test: `/` route correct response
     #[tokio::test]
     async fn test_root_route() {
         initialize_logging();
@@ -35,5 +36,33 @@ mod root_tests {
         assert_eq!(root_response.name, "blockfrost-platform");
         assert!(root_response.healthy);
         assert_eq!(root_response.sync_progress.percentage, 100.0);
+    }
+
+    // Test: `/tx/submit` route error
+    #[tokio::test]
+    async fn test_submit_route_error() {
+        initialize_logging();
+        let (app, _handle) = build_app().await.expect("Failed to build the application");
+
+        let tx = "8182068183051a000c275b1a000b35ec";
+        let body = Body::from(tx);
+
+        let request = Request::builder()
+            .method(Method::POST)
+            .uri("/tx/submit")
+            .header("Content-Type", "application/cbor")
+            .body(body)
+            .unwrap();
+
+        let response = app
+            .oneshot(request)
+            .await
+            .expect("Request to /tx/submit failed");
+
+        let cbor = to_bytes(response.into_body(), usize::MAX)
+            .await
+            .expect("Failed to read response body");
+
+        assert_eq!(cbor, "aaaa");
     }
 }
