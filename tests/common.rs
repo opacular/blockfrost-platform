2954c6c2d28881ee1a89f095081f5cbbba0e63ff
Vladimir Volek <volek.vladimir@gmail.com>
2025-01-22 18:59:11 +0100

    fix: lazy static and node env

--- tests/common.rs
+++ tests/common.rs
@@ -4,28 +4,31 @@ use blockfrost_platform::{
     server::build,
     AppError, NodePool,
 };
-use lazy_static::lazy_static;
-use std::sync::{Arc, Once};
+use std::{
+    env::var,
+    sync::{Arc, LazyLock},
+};
 use tower_http::normalize_path::NormalizePath;
 
-lazy_static! {
-    static ref INIT: Once = Once::new();
-}
+static INIT_LOGGING: LazyLock<()> = LazyLock::new(|| {
+    tracing_subscriber::fmt::init();
+});
 
 pub fn initialize_logging() {
-    INIT.call_once(|| {
-        tracing_subscriber::fmt::init();
-    });
+    let _ = INIT_LOGGING;
 }
 
 pub fn test_config() -> Arc<Config> {
+    let node_socket_path_env =
+        var("NODE_SOCKET_PATH").unwrap_or_else(|_| "/run/cardano-node/node.socket".into());
+
     let config = Config {
         server_address: "0.0.0.0".into(),
         server_port: 8080,
         log_level: LogLevel::Info.into(),
         network_magic: 2,
         mode: Mode::Compact,
-        node_socket_path: "/run/cardano-node/node.socket".into(),
+        node_socket_path: node_socket_path_env,
         icebreakers_config: None,
         max_pool_connections: 10,
         network: Network::Preview,
