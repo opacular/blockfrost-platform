c76f6c76d014afa6d025e2fdc80da3291ea33099
Michal Rus <michal.rus@iohk.io>
2026-06-11 17:11:51 +0200

    chore: update `dolos` to 1.2.0

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       flake = false; # otherwise, +2k dependencies we don’t really use
     };
     dolos = {
-      url = "github:txpipe/dolos/v1.1.1";
+      url = "github:txpipe/dolos/v1.2.0";
       flake = false;
     };
     blockfrost-tests = {
