7aa0b9c480430ff50d21fec04fd80bdf06b567ac
Michal Rus <michal.rus@iohk.io>
2026-06-22 13:47:22 +0200

    chore: update Dolos to 1.3.1
    This is just for the devshell and the desktop app.
    
    We still have to unignore the
    `epochs-latest-parameters-latest-epoch_a73a0c95ff80` test once Dolos is
    updated on the GitHub Runner.

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       flake = false; # otherwise, +2k dependencies we don’t really use
     };
     dolos = {
-      url = "github:txpipe/dolos/v1.2.0";
+      url = "github:txpipe/dolos/v1.3.1";
       flake = false;
     };
     blockfrost-tests = {
